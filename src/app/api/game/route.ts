9cddcf59011246f4cf0c68394ed6754f04505937
John-Anthony <johnanthonyelenis@gmail.com>
2025-07-11 23:26:07 -0400

    added default  empty game value

--- src/app/api/game/route.ts
+++ src/app/api/game/route.ts
@@ -7,7 +7,7 @@ export async function GET(request: Request) {
   
   const url : URL = new URL(request.url);
   let showNum = url.searchParams.get('show');
-  if (isNaN(Number(showNum)) || showNum == null) {
+  if (isNaN(Number(showNum)) || showNum == null || !showNum) {
     showNum = '1';
   }
   const game = await Jeopardy.getGameByShow(showNum);
