b8512c8fcbe51fd7be3afd385c957a82858e5f52
John-Anthony <johnanthonyelenis@gmail.com>
2025-10-20 14:05:18 -0400

    fixed line too long resume

--- src/app/jeopardy/api/current/route.ts
+++ src/app/jeopardy/api/current/route.ts
@@ -2,7 +2,8 @@ import { NextResponse } from 'next/server';
 const Jeopardy = require('jeopardy-json');
 
 export async function GET(request: Request) {
-  const list = await Jeopardy.getGamesList()
+  const list = await Jeopardy.update();
+
  
   return NextResponse.json(list.length);
 }
